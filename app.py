744dde9abefea3a534f0b314decbf132ef6fa15d
Ho Van Phat <hovanphat01@gmail.com>
2023-08-15 20:08:58 +0700

    Add warm algorithm

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from wit_fwi import WIT_FWI
 from wit_fwi_diff import WIT_FWI_DIFF
 from wis import generate_frequent_itemsets
+from warm import generate_association_rules
 import timeit
-from read_data import read_data, transform_to_data
 
 min_support = [1, 2, 3, 4, 5]
+min_confidence = 0.5
 database = [
     {'Beer': 3, 'Sausage': 2, 'Egg': 1},
     {'Beer': 1, 'Durian': 2, 'Yaourt': 2},
@@ -15,20 +16,24 @@ database = [
 runtime_WITs = []
 runtime_WIT_DIFFs = []
 runtime_WISs = []
+runtime_WARMs = []
 for i in min_support:
     runtime_WIT = timeit.timeit('WIT_FWI(database, i)', globals=globals(), number=1)
     runtime_WIT_DIFF = timeit.timeit('WIT_FWI_DIFF(database, i)', globals=globals(), number=1)
     runtime_WIS = timeit.timeit('generate_frequent_itemsets(database, i)', globals= globals(), number=1)
+    runtime_WARM = timeit.timeit('generate_association_rules(database, i, min_confidence)', globals=globals(), number=1)
 
     runtime_WITs.append(runtime_WIT)
     runtime_WIT_DIFFs.append(runtime_WIT_DIFF)
     runtime_WISs.append(runtime_WIS)
+    runtime_WARMs.append(runtime_WARM)
 
 
 import matplotlib.pyplot as plt
-plt.plot(min_support, runtime_WITs, 'red')
-plt.plot(min_support, runtime_WIT_DIFFs, 'blue')
-plt.plot(min_support, runtime_WISs, 'yellow')
+plt.plot(min_support, runtime_WITs, 'red', marker='o', label='WIT')
+plt.plot(min_support, runtime_WIT_DIFFs, 'blue', marker='o', label='WIT-DIFF')
+plt.plot(min_support, runtime_WISs, 'yellow', marker='o',label='WIS')
+plt.plot(min_support, runtime_WARMs, 'black', marker='o' ,label='WARM')
 plt.xlabel('min_supports')
 plt.ylabel('Runtimes')
 plt.title('Compare Algorithms')
--- warm.py
+++ warm.py
@@ -1,44 +1,117 @@
-# Sample transaction data have weighted mark
-transaction_data = [
-    {'items': ['A', 'B'], 'weight': 2},
-    {'items': ['A'], 'weight': 1},
-    {'items': ['B', 'C'], 'weight': 3},
-]
+# # Sample transaction data have weighted mark
+# # transaction_data = [
+# #     {'items': ['A', 'B'], 'weight': 2},
+# #     {'items': ['A'], 'weight': 1},
+# #     {'items': ['B', 'C'], 'weight': 3},
+# # ]
 
-# Step 1: Generate Frequent Itemsets
-min_support = 0.2
+# transaction_data = [
+#     {'Beer': 3, 'Sausage': 2, 'Egg': 1},
+#     {'Beer': 1, 'Durian': 2, 'Yaourt': 2},
+#     {'Beer': 2, 'Durian': 3, 'Yaourt': 2, 'Beef': 1},
+#     {'Durian': 1, 'Yaourt': 2, 'Beef': 2}
+# ]
 
-frequent_itemsets = {}
-for transaction in transaction_data:
-    for item in transaction['items']:
-        if item in frequent_itemsets:
-            frequent_itemsets[item] += transaction['weight']
-        else:
-            frequent_itemsets[item] = transaction['weight']
+# # Step 1: Generate Frequent Itemsets
+# # min_support = 0.2
+# min_support = 1
 
-frequent_itemsets = {item: support for item, support in frequent_itemsets.items() if support >= min_support}
+# frequent_itemsets = {}
+# for transaction in transaction_data:
+#     for item in transaction['items']:
+#         if item in frequent_itemsets:
+#             frequent_itemsets[item] += transaction['weight']
+#         else:
+#             frequent_itemsets[item] = transaction['weight']
 
-# Step 2: Generate Association Rules
-association_rules = []
-for itemset in frequent_itemsets:
-    for item in frequent_itemsets:
-        if item != itemset:
-            # Calculate confidence and lift
-            confidence = frequent_itemsets[itemset] / frequent_itemsets[item]
-            lift = frequent_itemsets[itemset] / (frequent_itemsets[item] * frequent_itemsets[itemset])
+# frequent_itemsets = {item: support for item, support in frequent_itemsets.items() if support >= min_support}
 
-            # Add rule to association_rules list
-            association_rules.append({
-                'itemset': itemset,
-                'item': item,
-                'confidence': confidence,
-                'lift': lift
-            })
+# # Step 2: Generate Association Rules
+# association_rules = []
+# for itemset in frequent_itemsets:
+#     for item in frequent_itemsets:
+#         if item != itemset:
+#             # Calculate confidence and lift
+#             confidence = frequent_itemsets[itemset] / frequent_itemsets[item]
+#             lift = frequent_itemsets[itemset] / (frequent_itemsets[item] * frequent_itemsets[itemset])
 
-# Step 3: Filtering Rules
-min_confidence = 0.5
-filtered_rules = [rule for rule in association_rules if rule['confidence'] >= min_confidence]
+#             # Add rule to association_rules list
+#             association_rules.append({
+#                 'itemset': itemset,
+#                 'item': item,
+#                 'confidence': confidence,
+#                 'lift': lift
+#             })
 
-# Print the filtered rules
-for rule in filtered_rules:
-    print(f"Rule: {rule['itemset']} => {rule['item']}, Confidence: {rule['confidence']}, Lift: {rule['lift']}")
+# # Step 3: Filtering Rules
+# min_confidence = 0.5
+# filtered_rules = [rule for rule in association_rules if rule['confidence'] >= min_confidence]
+
+# # Print the filtered rules
+# for rule in filtered_rules:
+#     print(f"Rule: {rule['itemset']} => {rule['item']}, Confidence: {rule['confidence']}, Lift: {rule['lift']}")
+
+from itertools import combinations
+from collections import defaultdict
+
+def generate_association_rules(transaction_data, min_support, min_confidence):
+    """
+    Tạo quy tắc kết hợp dựa trên dữ liệu giao dịch, hỗ trợ tối thiểu và độ tin cậy tối thiểu.
+    
+    Parameters:
+        transaction_data (list of dicts): Một danh sách các giao dịch, trong đó mỗi giao dịch là 
+                                          một từ điển với các mục là khóa và số lượng tương ứng là giá trị.
+        min_support (int): Ngưỡng hỗ trợ tối thiểu cho tập phổ biến.
+        min_confidence (float): Ngưỡng tin cậy tối thiểu cho các luật kết hợp.
+        
+    Returns:
+        Danh sách các luật kết hợp thỏa mãn tiêu chí độ hỗ trợ và độ tin cậy đã cho.
+    """
+    # Calculate support for each item
+    item_support = defaultdict(int)
+    for transaction in transaction_data:
+        for item_name, item_quantity in transaction.items():
+            item_support[item_name] += item_quantity
+    
+    # Filter items based on minimum support
+    frequent_items = {item: support for item, support in item_support.items() if support >= min_support}
+    
+    # Generate association rules
+    association_rules = []
+    for transaction in transaction_data:
+        for item_name, item_quantity in transaction.items():
+            for other_item in frequent_items:
+                if item_name != other_item:
+                    item_support_count = item_support[item_name]
+                    other_item_support_count = item_support[other_item]
+                    confidence = item_support_count / other_item_support_count
+                    lift = confidence / (item_support[other_item] / len(transaction_data))
+                    total_weight = item_quantity + item_support[other_item]
+                    association_rules.append({
+                        'itemset': {item_name},
+                        'item': {other_item},
+                        'confidence': confidence,
+                        'lift': lift,
+                        'total_weight': total_weight
+                    })
+
+    filtered_rules = [rule for rule in association_rules if rule['confidence'] >= min_confidence]
+    return filtered_rules
+
+# # Sample transaction data
+# transaction_data = [
+#     {'Beer': 3, 'Sausage': 2, 'Egg': 1},
+#     {'Beer': 1, 'Durian': 2, 'Yaourt': 2},
+#     {'Beer': 2, 'Durian': 3, 'Yaourt': 2, 'Beef': 1},
+#     {'Durian': 1, 'Yaourt': 2, 'Beef': 2}
+# ]
+
+# min_support = 0.5
+# min_confidence = 0.5
+
+# filtered_rules = generate_association_rules(transaction_data, min_support, min_confidence)
+
+# for rule in filtered_rules:
+#     itemset = ', '.join(rule['itemset'])
+#     item = ', '.join(rule['item'])
+#     print(f"Itemset: {itemset} => {item}, Confidence: {rule['confidence']}, Lift: {rule['lift']}, Total Weight: {rule['total_weight']}")
